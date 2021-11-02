0db7e3621f89ac6b80fa3f6c32590cd0a38477dc
rezkmohamed <momito310399@gmail.com>
2021-11-02 15:01:40 +0100

    added AuthFilter

--- src/main/java/com/scai/ecommerce/service/ProductService.java
+++ src/main/java/com/scai/ecommerce/service/ProductService.java
@@ -3,7 +3,6 @@ package com.scai.ecommerce.service;
 import java.util.List;
 
 import com.scai.ecommerce.dto.ProductDTO;
-import com.scai.ecommerce.entity.Product;
 
 public interface ProductService {
 	public List<ProductDTO> findProducts();
