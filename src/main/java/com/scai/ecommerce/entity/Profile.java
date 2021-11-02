5f71edd8df83ff57710ed7a814a5db5919393f3c
rezkmohamed <momito310399@gmail.com>
2021-11-02 11:19:55 +0100

    added Profile entity

---
+++ src/main/java/com/scai/ecommerce/entity/Profile.java
@@ -0,0 +1,75 @@
+package com.scai.ecommerce.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="profile")
+public class Profile {
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	@Column(name="id_profile")
+	private String idProfile;
+	
+	@Column(name="email")
+	private String email;
+
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="isvendor")
+	private boolean isVendor;
+	
+	public Profile() {}
+
+	public Profile(String email, String password, boolean isVendor) {
+		super();
+		this.email = email;
+		this.password = password;
+		this.isVendor = isVendor;
+	}
+
+	public String getIdProfile() {
+		return idProfile;
+	}
+
+	public void setIdProfile(String idProfile) {
+		this.idProfile = idProfile;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isVendor() {
+		return isVendor;
+	}
+
+	public void setVendor(boolean isVendor) {
+		this.isVendor = isVendor;
+	}
+
+	@Override
+	public String toString() {
+		return "Profile [idProfile=" + idProfile + ", email=" + email + ", password=" + password + ", isVendor="
+				+ isVendor + "]";
+	}
+}
