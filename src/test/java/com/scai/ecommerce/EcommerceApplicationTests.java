6f57a311eb299f7d9a19fbd559746e9bcc85a9ca
rezkmohamed <momito310399@gmail.com>
2021-11-02 11:10:43 +0100

    initial commit

---
+++ src/test/java/com/scai/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scai.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
