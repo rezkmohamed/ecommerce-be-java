39b6cf596e5b67e58050aa5c50f70f862b25546f
rezkmohamed <momito310399@gmail.com>
2021-11-02 15:15:57 +0100

    added AuthController

--- src/main/java/com/scai/ecommerce/controller/AuhtController.java
+++ src/main/java/com/scai/ecommerce/controller/AuhtController.java
@@ -1,5 +1,54 @@
 package com.scai.ecommerce.controller;
 
-public class AuhtController {
+import java.util.Date;
+import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.scai.ecommerce.dto.ProfileDTO;
+import com.scai.ecommerce.dto.UserDTO;
+import com.scai.ecommerce.service.ProfileService;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@RestController("")
+public class AuhtController {
+	@Autowired
+	private ProfileService profileService;
+	@Value("${signingKey}")
+	private String signingKey;
+	
+	
+	@PostMapping("/login")
+	public ResponseEntity<ProfileDTO> login(@RequestBody UserDTO user){
+		ProfileDTO profileToLogin = profileService.findProfileByEmailAndPassword(user.getEmail(), user.getPassword());
+		
+		if(profileToLogin != null) {
+			HttpHeaders headers = new HttpHeaders();
+        	HashMap<String, Object> addedValues = new HashMap<String, Object>();
+        	
+        	String token = Jwts.builder()
+        			.addClaims(addedValues)
+        			.setIssuedAt(new Date(System.currentTimeMillis()))
+					.setExpiration(new Date(System.currentTimeMillis() + 4 * 120 * 60 * 1000))
+					.signWith(SignatureAlgorithm.HS512, this.signingKey).compact();
+        	headers.add("Authentication","Bearer " + token);
+			
+			return ResponseEntity.ok().headers(headers).build();        	
+		}
+		
+		
+		
+		
+		return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+	}
+	
 }
