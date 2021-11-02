bc237bc5835373f22e822d1fe2a837f49cd8d820
rezkmohamed <momito310399@gmail.com>
2021-11-02 16:24:32 +0100

    refactor ProductController

--- src/main/java/com/scai/ecommerce/controller/ProductController.java
+++ src/main/java/com/scai/ecommerce/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,6 +38,35 @@ public class ProductController {
 		return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
 	}
 	
+	@GetMapping("/category/${category}")
+	public ResponseEntity<List<ProductDTO>> getProductsByCategory(@PathVariable String category){
+		List<ProductDTO> products = productService.findProductsByCategory(category);
+		if(products != null) {
+			return new ResponseEntity<>(products, HttpStatus.OK);
+		}
+		
+		return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+	}
+	
+	@GetMapping("/namelike/${nameLike}")
+	public ResponseEntity<List<ProductDTO>> getProductsByNameLike(@PathVariable String nameLike){
+		List<ProductDTO> products = productService.findProductByNameLike(nameLike);
+		if(products != null) {
+			return new ResponseEntity<>(products, HttpStatus.OK);
+		}
+		
+		return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+	}
+	
+	@GetMapping("/{idProduct}")
+	public ResponseEntity<ProductDTO> getProductById(@PathVariable String idProduct){
+		ProductDTO product = productService.findProductById(idProduct);
+		if(product != null) {
+			return new ResponseEntity<>(product, HttpStatus.OK);
+		}
+		
+		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+	}
 	
 	@PostMapping("/new")
 	public ResponseEntity<Boolean> addProduct(@RequestBody ProductDTO product, HttpServletRequest request){
@@ -48,6 +78,4 @@ public class ProductController {
 		
 		return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
 	}
-	
-	
 }
